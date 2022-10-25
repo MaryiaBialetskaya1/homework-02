2aed4e3832e0f59d15e112b96245b4767bc15055
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-25 15:55:48 -0400

    test commit

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,17 @@ app.use(jsonBodyMiddleware)
 
 const port = 3000
 
+let videos = [
+    {id:1, title: '1'},
+    {id:2, title: '2'},
+    {id:3, title: '3'},
+]
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello Homework-02 from IT-Incubator!!!!')
 })
-
+app.get('/videos', (req:Request, res:Response) => {
+    res.send(videos)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
