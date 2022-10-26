47151ac0521353e9cb93c299063c831720118441
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-26 11:35:03 -0400

    add get and post method for blogs

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,41 @@ app.use(jsonBodyMiddleware)
 
 const port = process.env.PORT || 3000
 
-let videos = [
-    {id:1, title: '1'},
-    {id:2, title: '2'},
-    {id:3, title: '3'},
-]
+let posts: any = []
+let blogs: any = []
+
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello Homework-02 from IT-Incubator!!!!')
 })
-app.get('/videos', (req:Request, res:Response) => {
-    res.send(videos)
+
+app.get('/blogs', (req:Request, res: Response) =>{
+    res.send(blogs);
+})
+app.post('/blogs', (req:Request ,res: Response) =>{
+    const error = []
+
+    let name = req.body.name
+    let youtubeUrl = req.body.youtubeUrl
+
+    if(!name || typeof name !== "string" || !name.trim() || name.length > 15){
+        error.push({
+            "message": "Incorrect name",
+            "field": "name"
+        })
+    }
+
+    if(error.length){
+        res.status(400).send({errorsMessages: error})
+        return;
+    }
+
+    const newBlog = {
+        name: req.body.name,
+        youtubeUrl: req.body.youtubeUrl
+    }
+    blogs.push(newBlog)
+    res.status(201).send(newBlog)
+
 })
 
 app.listen(port, () => {
