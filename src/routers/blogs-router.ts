62292a42ff4c324ea5566cef6b32fca7b11acc74
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-06 13:01:07 -0500

    edit errors

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -20,25 +20,25 @@ blogsRouter.get('/', (req:Request, res: Response) =>{
 })
 
 blogsRouter.post('/',
-    body('name')
-        .isString().withMessage('name should be string')
-        .trim().withMessage('name should be symbols string')
-        .notEmpty().withMessage('name is required')
-        .isLength({ max: 15 }).withMessage('max length of name is 15'),
-
-    body('youtubeUrl')
-        .isString().withMessage('youtubeUrl should be string')
-        .trim().withMessage('youtubeUrl should be symbols string')
-        .notEmpty().withMessage('youtubeUrl is required')
-        .isLength({ max: 15 }).withMessage('youtubeUrl length of name is 15')
-        .isURL().withMessage('youtubeUrl be valid URL'),
-
+    // body('name')
+    //     .isString().withMessage('name should be string')
+    //     .trim().withMessage('name should be symbols string')
+    //     .notEmpty().withMessage('name is required')
+    //     .isLength({ max: 15 }).withMessage('max length of name is 15'),
+    //
+    // body('youtubeUrl')
+    //     .isString().withMessage('youtubeUrl should be string')
+    //     .trim().withMessage('youtubeUrl should be symbols string')
+    //     .notEmpty().withMessage('youtubeUrl is required')
+    //     .isLength({ max: 15 }).withMessage('youtubeUrl length of name is 15')
+    //     .isURL().withMessage('youtubeUrl be valid URL'),
+    //
     (req:Request ,res: Response) =>{
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).json({ errors: errors.array() });
+    // }
 
 
     const newBlog = blogsRepository.createBlog(req.body.name, req.body.youtubeUrl);
