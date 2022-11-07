1c4cd1d2a6c0b1a8e130ac5fb860a1b1d81ea7cb
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-06 22:08:42 -0500

    edit blog validation middleware

--- src/middlewares/blogValidationMiddleware.ts
+++ src/middlewares/blogValidationMiddleware.ts
@@ -1,17 +1,5 @@
 import {body} from "express-validator";
 
-// const nameValidation = body('name')
-//     .isString().withMessage('invalid name isString')
-//     .trim().withMessage('invalid name trim')
-//    // .notEmpty().withMessage('invalid name notEmpty')
-//     .isLength({ max: 15 }).withMessage('invalid name isLength')
-const nameValidation = body('name').trim().isLength({min:1, max: 15}).isString()
-const youtubeUrlValidation = body('youtubeUrl').trim().isLength({min:1, max: 100}).isString().matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
-// const youtubeUrlValidation = body('youtubeUrl')
-//     .isString().withMessage('invalid youtubeUrl isString')
-//     .trim().withMessage('invalid youtubeUrl trim')
-//     .notEmpty().withMessage('invalid youtubeUrl notEmpty')
-//     .isLength({ max: 100 }).withMessage('invalid youtubeUrl isLength')
-//     .isURL().withMessage('invalid isURL');
+export const nameValidation = body('name').trim().isLength({min:1, max: 15}).isString()
+export const youtubeUrlValidation = body('youtubeUrl').trim().isLength({min:1, max: 100}).isString().matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
 
-export {nameValidation, youtubeUrlValidation};
