4c66c275c4c64e19415f562fc43f440a7530c4e4
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-06 22:20:06 -0500

    edit input validation middleware

--- src/middlewares/inputValidationMiddleware.ts
+++ src/middlewares/inputValidationMiddleware.ts
@@ -1,20 +1,20 @@
-
-import {validationResult} from "express-validator";
 import {NextFunction, Request, Response} from "express";
+import {validationResult} from "express-validator";
 
-const myValidationResult = validationResult.withDefaults({
+
+const errorFormatter = validationResult.withDefaults({
     formatter: error => {
         return{
             message: error.msg,
             field: error.param,
-        }
-    }
-})
+        };
+    },
+});
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) =>{
-    const errorsMessages = myValidationResult(req);
+    const errorsMessages = errorFormatter(req);
     if (!errorsMessages.isEmpty()) {
-        return res.status(400).json({ errorsMessages: errorsMessages.array() });
+        return res.status(400).json({ errorsMessages: errorsMessages.array({onlyFirstError:true})});
     } else{
         next()
     }
