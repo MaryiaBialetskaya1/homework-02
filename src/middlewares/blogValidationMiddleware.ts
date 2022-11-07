6c6e0eb62ece9bf1ca9f358da996662d11cca45f
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-06 22:06:21 -0500

    edit blog validation middleware

--- src/middlewares/blogValidationMiddleware.ts
+++ src/middlewares/blogValidationMiddleware.ts
@@ -1,11 +1,11 @@
 import {body} from "express-validator";
 
-const nameValidation = body('name')
-    .isString().withMessage('invalid name isString')
-    .trim().withMessage('invalid name trim')
-   // .notEmpty().withMessage('invalid name notEmpty')
-    .isLength({ max: 15 }).withMessage('invalid name isLength')
-
+// const nameValidation = body('name')
+//     .isString().withMessage('invalid name isString')
+//     .trim().withMessage('invalid name trim')
+//    // .notEmpty().withMessage('invalid name notEmpty')
+//     .isLength({ max: 15 }).withMessage('invalid name isLength')
+const nameValidation = body('name').trim().isLength({min:1, max: 15}).isString()
 const youtubeUrlValidation = body('youtubeUrl').trim().isLength({min:1, max: 100}).isString().matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
 // const youtubeUrlValidation = body('youtubeUrl')
 //     .isString().withMessage('invalid youtubeUrl isString')
