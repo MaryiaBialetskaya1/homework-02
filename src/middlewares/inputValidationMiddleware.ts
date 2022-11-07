8b8230e02684d3417ca4bfe341f4b50a8ca5ad15
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-06 21:09:56 -0500

    edit input validation middleware

--- src/middlewares/inputValidationMiddleware.ts
+++ src/middlewares/inputValidationMiddleware.ts
@@ -1,5 +1,6 @@
-import {NextFunction, Request, Response} from "express";
+
 import {validationResult} from "express-validator";
+import {NextFunction, Request, Response} from "express";
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) =>{
     const errorsMessages = validationResult(req);
