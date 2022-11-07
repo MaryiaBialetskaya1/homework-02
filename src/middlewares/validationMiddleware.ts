0cf590632f84e90cfcbb8121c8f6b0e06f4f8141
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-06 22:50:37 -0500

    add validation check errors

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,5 +1,37 @@
 import {body} from "express-validator";
 
-export const nameValidation = body('name').trim().isLength({min:1, max: 15}).isString()
-export const youtubeUrlValidation = body('youtubeUrl').trim().isLength({min:1, max: 100}).isString().matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
+export const nameValidation = body('name')
+    .trim()
+    .isLength({min:1, max: 15})
+    .isString()
+
+export const youtubeUrlValidation = body('youtubeUrl')
+    .trim().
+    isLength({min:1, max: 100})
+    .isString()
+    .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
+
+export const titleValidation = body('title')
+    .trim()
+    .notEmpty()
+    .isLength({max: 300})
+    .isString()
+
+export const shortDescriptionValidation = body('shortDescription')
+    .trim()
+    .notEmpty()
+    .isLength({max: 100})
+    .isString()
+
+export const contentValidation = body('content')
+    .trim()
+    .notEmpty()
+    .isLength({max:1000})
+    .isString()
+
+export const bodyBlogIdValidation = body('blogId')
+    .trim()
+    .isLength({min: 13, max:13})
+    .notEmpty()
+    .isString()
 
