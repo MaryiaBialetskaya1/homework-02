9e2f7b8bfae726066b226caa64940ab12683539d
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-17 14:41:09 -0500

    edit authorization

--- src/middlewares/checkAuthorizationMiddleware.ts
+++ src/middlewares/checkAuthorizationMiddleware.ts
@@ -1,21 +1,21 @@
 import {NextFunction, Request, Response} from "express";
 import {atob} from "buffer";
 
-// export const checkAuthorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-//     const authorization = req.header("Authorization");
-//     if(!authorization?.startsWith("Basic")){
-//         return res.sendStatus(401);
-//     }
-//     try{
-//         const[login, pass] = atob(authorization?.split(" ")[1]).split(":");
-//         if(login !== "admin" || pass !== "qwerty"){
-//             return res.sendStatus(401);
-//         } else {
-//             next();
-//         }
-//     } catch (e){
-//         return res.sendStatus(401)
-//     }
-// }
+export const checkAuthorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authorization = req.header("Authorization");
+    if(!authorization?.startsWith("Basic")){
+        return res.sendStatus(401);
+    }
+    try{
+        const[login, pass] = atob(authorization?.split(" ")[1]).split(":");
+        if(login !== "admin" || pass !== "qwerty"){
+            return res.sendStatus(401);
+        } else {
+            next();
+        }
+    } catch (e){
+        return res.sendStatus(401)
+    }
+}
 
 
