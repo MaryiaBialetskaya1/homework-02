b64ebff6c1239d7282fb7569aa793c8f23b62d33
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-12-15 21:03:55 -0500

    work on blogs-service

---
+++ src/domain/blogs-service.ts
@@ -0,0 +1,9 @@
+import { blogsRepository } from "../repositories/blogs-repository";
+
+type TypeNewBlog = {
+    name: string
+    description: string
+    websiteUrl: string
+    createdAt: string
+}
+
