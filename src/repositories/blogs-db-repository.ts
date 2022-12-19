ed5c929c05c0352946899e0ebf20c16d593a56a5
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-12-19 15:10:13 -0500

    add semicolons in  blogs-db-repository

--- src/repositories/blogs-db-repository.ts
+++ src/repositories/blogs-db-repository.ts
@@ -6,35 +6,29 @@ export const blogsRepository = {
      async findBlogs() : Promise<blogsType[]>{
          return blogCollection.find({}, {projection:{_id:0}}).toArray();
     },
-
     async createBlog(newBlog: blogsType ): Promise<string>{
          const result = await blogCollection.insertOne(newBlog);
          return result.insertedId.toString();
     },
-
     async findBlogById(id: string): Promise<blogsType | null>{
         const blog = await blogCollection.findOne({_id: new ObjectId(id)})
         if(!blog){
-            return null
+            return null;
         }
         return blog;
     },
-
     async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean>{
         const result = await blogCollection.updateOne(
             {_id: new ObjectId(id)},
             {$set: {name, description, websiteUrl}}
-        )
-        return result.matchedCount === 1
+        );
+        return result.matchedCount === 1;
     },
-
     async deleteBlog(id: string): Promise<boolean>{
-        const result = await blogCollection.deleteOne({_id: new ObjectId(id)})
-        return result.deletedCount === 1
+        const result = await blogCollection.deleteOne({_id: new ObjectId(id)});
+        return result.deletedCount === 1;
     },
-
     async deleteAll(){
          await blogCollection.deleteMany({});
     }
-
 }
