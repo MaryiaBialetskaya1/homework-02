84edb153c48e4617cf8f7bc76311e108a51c5ef8
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-12-21 14:37:03 -0500

    edit file blogs-queryRepo

---
+++ src/repositories/blogs-queryRepo.ts
@@ -0,0 +1,39 @@
+import { blogCollection } from "./db";
+import { ObjectId } from "mongodb";
+
+type TypeBlog = {
+    id: string
+    name: string
+    description: string
+    websiteUrl: string
+    createdAt: string
+};
+
+type TypeBlogDb = {
+    _id: ObjectId
+    name: string
+    description: string
+    websiteUrl: string
+    createdAt: string
+};
+
+export const blogsQueryRepo = {
+    async findBlogsById(id: string): Promise<TypeBlog | null>{
+        const foundBlog = await blogCollection.findOne({_id: new ObjectId(id)})
+            if(!foundBlog?._id){
+                return null
+            } else{
+                return this.blogWithReplaceId(foundBlog)
+        }
+    },
+
+    blogWithReplaceId (object: TypeBlogDb): TypeBlog{
+        return {
+            id: object._id.toString(),
+            name: object.name,
+            description: object.description,
+            websiteUrl: object.websiteUrl,
+            createdAt: object.createdAt
+        }
+    }
+}
