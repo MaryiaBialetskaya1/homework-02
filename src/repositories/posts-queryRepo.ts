1c8063b81053c02caad8021b644550e5922870c7
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-09 13:11:18 -0500

    edit file posts-queryRepo.ts

--- src/repositories/posts-queryRepo.ts
+++ src/repositories/posts-queryRepo.ts
@@ -24,7 +24,7 @@ type PostDbType = {
 
 export  const postsQueryRepo = {
     async getAllPosts(pageNumber: number, pageSize: number, sortBy: string, sortDirection: 'asc' | 'desc'){
-        const totalCount = await postCollection.countDocuments({pageNumber, pageSize});
+        const totalCount = await postCollection.countDocuments({pageNumber: pageNumber, pageSize: pageSize});
         const posts = await postCollection
             .find({})
             .skip(getSkippedNumber(pageNumber, pageSize))
