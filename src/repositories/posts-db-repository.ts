325176f3760df77de01989a747bea70b1f7e8d25
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-09 11:02:21 -0500

    delete commented code

--- src/repositories/posts-db-repository.ts
+++ src/repositories/posts-db-repository.ts
@@ -4,17 +4,6 @@ import {ObjectId} from "mongodb";
 export let posts: postsType[] = [];
 
 export const postsRepository = {
-    // async findPosts() : Promise<postsType[]> {
-    //     return postCollection.find({}, ).toArray();
-    // },
-
-    // async findPostById(id: string): Promise<postsType | null> {
-    //     const post = await postCollection.findOne({_id: new ObjectId(id)})
-    //     if (!post){
-    //         return null;
-    //     }
-    //     return post;
-    // },
 
     async createPost(newPost: postsType): Promise<string>{
         const result = await postCollection.insertOne(newPost);
